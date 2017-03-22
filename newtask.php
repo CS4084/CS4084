3555e3d6b0ec26e2241bde40c9b805cf31852b1f
colmlg <colm.legear@gmail.com>
2017-03-22 16:12:53 +0000

    New Task Page

---
+++ newtask.php
@@ -0,0 +1,205 @@
+<?php 
+  include('config.php');
+  session_start();
+  
+  if ( $_SESSION['userId']=="") {
+   header("Location: index.php");
+   exit;
+  }
+  
+   $error = false;
+ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	 
+	if(isset($_POST['title']) && isset($_POST['type']) && isset($_POST['description']) && isset($_POST['pagecount']) && isset($_POST['wordcount']) && isset($_POST['claimdeadline']) && isset($_POST['completiondeadline']) && isset($_POST['taskfile']))
+	{
+		
+	}
+	
+	else
+		$error = true;	 
+ }
+  
+  
+  
+
+?>
+
+
+<!DOCTYPE html>
+<!-- Template by Quackit.com -->
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Create a New Task - Proofreaders</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles -->
+    <link href="css/custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+        <div class="container">
+            <!-- Logo and responsive toggle -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-education"></span> Proofreaders</a>
+            </div>
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="dashboard.html">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="profile.html">My Profile</a>
+                    </li>
+                    <li>
+                        <a href="claimedtasks.html">Claimed Tasks</a>
+                    </li>
+					   
+                </ul>
+
+				<!-- Search -->
+				<form class="navbar-form navbar-right" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control">
+					</div>
+					<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span> Search</button>
+				</form>
+
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+<div class="container-fluid">
+
+		<!-- Left Column -->
+		<div class="col-sm-3">
+
+			<!-- List-Group Panel -->
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h1 class="panel-title"><span class="glyphicon glyphicon-home"></span> Dashboard</h1>
+				</div>
+				<div class="list-group">
+					<a href="logout.php" class="list-group-item">Log out</a>
+					<a href="newtask.php" class="list-group-item">Create a new task</a>
+				
+				</div>
+			</div>
+
+	
+		</div><!--/Left Column-->
+  
+  
+		<!-- Center Column -->
+		<div class="col-sm-6">
+			
+		<div class="panel panel-default">
+		  <div class="panel-heading">
+			<h3 class="panel-title">Create New Task</h3>
+		  </div>
+		  <div class="panel-body">
+		  
+ 
+
+		
+			<form>
+					<div class="form-group">
+						<input type="text" class="form-control" id="title" name="title" placeholder="Task title">
+					</div>
+					
+					<div class="form-group">
+						<input type="text" class="form-control" id="type" name="type" placeholder="Task Type (e.g. Thesis, Project)">
+					</div>
+					
+					
+					<div class="form-group">
+						<textarea class="form-control" rows="8" id="description" name="decription" placeholder="Task Description"></textarea>
+					</div>
+					<div class="form-group">
+						<p>Task Subject:</p>
+							<select class="form-control" name="discipline" id="discipline">
+								<option value="one">Education and Health Sciences</option>
+								<option value="two">Arts and Humanities</option>
+								<option value="three">Science and Engineering</option>
+								<option value="four">Business</option>
+							</select>
+						</div>
+					<div class="form-group">
+					
+						<input type="number" class="form-control" id="pagecount" name="pagecount" placeholder="Page Count">
+					</div>
+					<div class="form-group">
+					
+						<input type="number" class="form-control" id="wordcount" name="wordcount" placeholder="Word Count">
+					</div>
+					
+					<div class="form-group">
+					<p>Task Claim Deadline (Task will be unpublished if not claimed by this date.)</p>
+						<input type="date" class="form-control" id="claimdeadline" name="claimdeadline">
+					</div>
+					<div class="form-group">
+					<p>Task Completetion Deadline</p>
+						<input type="date" class="form-control" id="completiondeadline" name="completiondeadline">
+						
+					</div>
+					<div class="form-group">
+						<input type="file" name="taskfile" />
+					</div>
+					
+					<button type="submit" class="btn btn-default">Create Task</button>
+				</form>
+			
+			
+			
+			
+			</div> 
+			  
+			  
+		  </div>
+		</div>
+		</div>
+			
+			<hr>
+		</div><!--/Center Column-->
+
+
+	</div><!--/container-fluid-->
+	
+	<footer>
+		
+        
+        <div class="small-print">
+        	<div class="container">
+        		<p><a href="#">Terms &amp; Conditions</a> | <a href="#">Privacy Policy</a> | <a href="#">Contact</a></p>
+        		<p>Copyright &copy; Example.com 2015 </p>
+        	</div>
+        </div>
+	</footer>
+
+	
+    <!-- jQuery -->
+    <script src="js/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	
+	
+</body>
+
+</html>
