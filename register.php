debcae8b6f8344f69f0951b5c6dc1112c346ae6f
Liam-Horgan <14164736@studentmail.ul.ie>
2017-03-27 14:46:04 +0100

    php

--- register.php
+++ register.php
@@ -2,74 +2,92 @@
 //ob_start();
 session_start();
  include('config.php');
+ session_start();
  
  if ( isset($_SESSION['userId'])!="" ) {
   header("Location: dashboard.php");
   exit;
- }
-$errormsg = "";
+  
 $error = false;
-
  if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	 
-	 $todaydate =  date("Y-m-d");
 	// clean user inputs to prevent sql injections
-	 $email = mysqli_real_escape_string($db,$_POST['email']);
-     $pwd = mysqli_real_escape_string($db,$_POST['pwd']); 
-	 $firstname = mysqli_real_escape_string($db, $_POST['firstname']);
-	 $lastname = mysqli_real_escape_string($db, $_POST['lastname']);
-	 $id = mysqli_real_escape_string($db, $_POST['id']);
+	
+	$firstname = trim($_POST['firstname']);
+	$firstname = strip_tags($firstname);
+	$firstname = htmlspecialchars($firstname);
+	
+	$lastname = trim($_POST['lastname']);
+	$lastname = strip_tags($lastname);
+	$lastname = htmlspecialchars($lastname);
+	
+	$email = trim($_POST['email']);
+	$email = strip_tags($email);
+	$email = htmlspecialchars($email);
+	
+	$pwd = trim($_POST['pwd']);
+	$pwd = strip_tags($pwd);
+	$pwd = htmlspecialchars($pwd);
+	
+	$id = trim($_POST['id']);
+	$id = strip_tags($id);
+	$id = htmlspecialchars($id);
 	
 	// email validation
 	if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
 		$error = true;
-		$errormsg .= "Please enter valid email address.";
+		$emailError = "Please enter valid email address.";
 	} else {
 		//  email exist or not
-		$sql = "SELECT email FROM users WHERE email='$email'";
-		$result = mysqli_query($db,$sql)
-		or die("Error: ".mysqli_error($db));
-		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-		$count = mysqli_num_rows($result);
-      
-		// If result matched $myusername and $mypassword, table row must be 1 row
-		
-		if($count > 0) {
-         $error = true;
-      }
+		$query = "SELECT userEmail FROM users WHERE userEmail='$email'";
+		$result = mysql_query($query);
+		$count = mysql_num_rows($result);
+		if($count!=0){
+			$error = true;
+			$emailError = "Provided Email is already in use.";
+		}
 	}
 	
 	//studentID val
 	if(empty($id)){
 		$error=true;
-		$errormsg .="Please enter youur student ID.";
+		$studentIDError="Please enter youur student ID.";
 	}
 	else if(strlen($id)!=8){
 		$error=true;
-		$errormsg .="Must be 8 characters long.";
+		$studentIDError="Must be 8 characters long.";
 	}
 	else if(!preg_match("/^(?:0|[1-9][0-9]*)$/",$id)){
-		$error=true;
-		$errormsg .="May only contain numbers.";
+		$id=true;
+		$studentIDError="May only contain numbers.";
 	}
 	// password validation
 	if (empty($pwd)){
 		$error = true;
-		$errormsg .= "Please enter password.";
+		$passError = "Please enter password.";
 	} else if(strlen($pwd) < 6) {
 		$error = true;
-		$errormsg .= "Password must have atleast 6 characters.";
+		$passError = "Password must have atleast 6 characters.";
 	}
 	// password encrypt using SHA256();
 	$password = hash('sha256', $pwd);
 	if( !$error ) {
-		$query = "INSERT INTO users(firstName,lastName,studentId,email,password,dateJoined) VALUES('$firstname','$lastname','$id','$email','$password','$todaydate')";
-		mysqli_query($db,$query);
-		$taskId = mysqli_insert_id($db);
-		header("location: index.php?success=1");
+		$query = "INSERT INTO users(firstName,lastName,id,userEmail,userPass) VALUES('$firstname','$lastname','$id','$email','$pwd')";
+		$res = mysql_query($query);
+		if ($res) {
+			$errTyp = "success";
+			$errMSG = "Successfully registered, you may login now";
+			unset($firstname);
+			unset($lastname);
+			unset($id);
+			unset($email);
+			unset($pwd);
+		} else {
+			$errTyp = "danger";
+			$errMSG = "Something went wrong, try again later...";
+		}
 	}
-} 
-$errorbox = "<div class='alert alert-danger' role='alert'><span class='glyphicon glyphicon-warning-sign'></span>  Error: Please try again. $errormsg</div>";
+}
 ?>
 
 <html lang="en"><head>
@@ -114,11 +132,6 @@ $errorbox = "<div class='alert alert-danger' role='alert'><span class='glyphicon
 					</h3>
 				</div>
 				<div class="panel-body">
-				<?php
-				if($error){
-					echo($errorbox);
-				}
-				?>
 					<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 						<div class="form-group">
 							<input type="text" class="form-control" id="email" name="email" placeholder="Email">
@@ -135,10 +148,10 @@ $errorbox = "<div class='alert alert-danger' role='alert'><span class='glyphicon
 						<div class="form-group">
 						<p>Select a discipline</p>
 							<select class="form-control" name="discipline" id="discipline">
-								<option value="Education and Health Sciences">Education and Health Sciences</option>
-								<option value="Arts and Humanities">Arts and Humanities</option>
-								<option value="Science and Engineering">Science and Engineering</option>
-								<option value="Business">Business</option>
+								<option value="one">Education and Health Sciences</option>
+								<option value="two">Arts and Humanities</option>
+								<option value="three">Science and Engineering</option>
+								<option value="four">Business</option>
 							</select>
 						</div>			
 						<div class="form-group">
@@ -156,5 +169,5 @@ $errorbox = "<div class='alert alert-danger' role='alert'><span class='glyphicon
 	  </div>
 
 	</div><!--/container-fluid-->
-	</body>
-</html>
+	
+	
